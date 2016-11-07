8d107df03c6ade766e8ad59ae366f540505bf439
bravelcy <18258283716@163.com>
2016-11-07 22:41:25 +0800

    fab

---
+++ fab_1.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+def fab(n):
+  if n<1:
+     print ('输入出错！')  #斐波那契数列，迭代思想，好像比递归快
+  
+  if n==1 or n==2:
+     return 1
+  else:
+     return fab(n-1)+fab(n-2)
+result=fab(20)
+if result !=-1:
+    print u"总共生下的小兔崽子的数量%d"%result
