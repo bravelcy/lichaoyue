07847a4707346e33bb24afb36d30c98b0c2f5251
bravelcy <18258283716@163.com>
2016-11-08 21:00:50 +0800

    关于集合的一些用法
    还不够全，补了一些用法

--- 集合.py
+++ 集合.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 num1={1,2,3,4,5,5,4,3,2,1,0}  #集合里面的数是唯一的
+num3={11,12,13}
 print num1                  #打印出来的数就是去掉了重复的
 # for in 和for not in 
+num1.add(8)                   #add为添加元素到集合里面，发现只能添加一个元素
+print num1
+num1.update(num3)              #update 将一个字典添加到另一个字典里面
+print num1
 
 temp=[]
 for each in num1:              #去掉重复元素
@@ -20,3 +25,6 @@ num2=frozenset(num1)             #冰冻，不可变集合
 print num2
 num2.remove(5)                  #AttributeError: 'frozenset' object has no attribute 'remove',报错，不能删除
 print num2
+
+
+
