878bbb378cda70dbf7dac966a86e487c4cfe1395
bravelcy <18258283716@163.com>
2016-12-10 21:35:42 +0800

    字典，内置函数的应用

--- 存储查找名字.py
+++ 存储查找名字.py
@@ -33,6 +33,11 @@ print lookup(MyNames,'first','Li')
 #否则就根据name加入这个全名
 #例如:
 #data['first'] = {'Ma': ['Ma Li He']}
-#如果新来一个叫Ma Li Zhang的就变成了data['first'] = {'Ma': ['Ma Li He', 'Ma Li Zhang']}
-#要是一个 San Zhang的, 因为San不在data['first']里, 所以要新添加一个San的key进去, 就变成了
-#data['first'] = {'Ma': ['Ma Li He', 'Ma Li Zhang'], 'San': ['San Zhang']}'
+如果新来一个叫Ma Li Zhang的就变成了data['first'] = {'Ma': ['Ma Li He', 'Ma Li Zhang']}
+要是一个 San Zhang的, 因为San不在data['first']里, 所以要新添加一个San的key进去, 就变成了
+data['first'] = {'Ma': ['Ma Li He', 'Ma Li Zhang'], 'San': ['San Zhang']}'
+这个可以理解为拉链，将两个list拉到一起来，每个对应元素做一定操作后，合并成一个list.
+比如：
+zip([1,2,3],['a','b','c'])
+结果是
+[(1, 'a'), (2, 'b'), (3, 'c')]
